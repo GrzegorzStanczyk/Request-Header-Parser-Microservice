09e31a656ea8cf42773149f2321cef11de3569b2
Future Front-End Developer <grzegorz.stanczyk@o2.pl>
2018-04-01 18:39:14 +0200

    Project upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Request-Header-Parser-Microservice
+
+Live [https://mirage-pull.glitch.me/](https://mirage-pull.glitch.me/)
+
+##User Story: 
+
+I can get the IP address, language and operating system for my browser.
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.get("/", function (req, res) {
+  const ip = req.header('x-forwarded-for').split(',')[0];
+  const lang = req.header('accept-language').split(',')[0];
+  const userAgent = req.header('user-agent');
+  const software = userAgent.slice(userAgent.indexOf('(') + 1, userAgent.indexOf(')'));
+  res.json({
+    'ipaddress': ip,
+    'language': lang,
+    'software': software
+  })
+});
+
+app.use((req, res, next) => {
+  res.status(404).end('404');
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
